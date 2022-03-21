a4c0a80099448508e0865d3c5ccbda6031a04f81
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-21 18:19:53 -0300

    [CHG] se saco la contraseña de todas las respuestas

--- models/usuario.js
+++ models/usuario.js
@@ -29,7 +29,7 @@ const UsuarioSchema = Schema({
 });
 
 UsuarioSchema.method('toJSON', function() {
-  const { __v, _id, ...object } = this.toObject();
+  const { __v, _id, password, ...object } = this.toObject();
   object.uid = _id;
   return object;
 });

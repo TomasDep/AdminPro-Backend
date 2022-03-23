b4724243597c66ae85d138ea21233621b0362b86
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-23 19:58:20 -0300

    [CHG] console log borrado

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -8,7 +8,6 @@ const googleVerify = async (token) => {
   });
   const payload = ticket.getPayload();
   const userid = payload['sub'];
-  console.log(payload);
   const { name, email, picture } = payload;
 
   return { name, email, picture };

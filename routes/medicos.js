da0d431d713c2977937a3c84cd4115e277be647a
TomasDep <tomas.valenzuelacb@gmail.com>
2022-04-05 15:09:15 -0400

    [ADD] se agrega ruta de medicoPorId y se arrega error de check hospital

--- routes/medicos.js
+++ routes/medicos.js
@@ -6,11 +6,16 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { getMedicos, crearMedicos, actualizarMedicos, borrarMedicos } = require('../controllers/medicos');
+const { getMedicos, 
+        crearMedicos, 
+        actualizarMedicos, 
+        borrarMedicos, 
+        getMedicoPorId 
+} = require('../controllers/medicos');
 
 const router = Router();
 
-router.get('/', getMedicos);
+router.get('/', validarJWT,getMedicos);
 router.post('/', [
   validarJWT,
   check('nombre', 'El nombre del medico es requerido').not().isEmpty(),
@@ -20,9 +25,10 @@ router.post('/', [
 router.put('/:id', [ 
   validarJWT,
   check('nombre', 'El nombre del medico es requerido').not().isEmpty(),
-  check('hospital', 'El nombre del medico es requerido').isMongoId(),
+  check('hospital', 'El hospital es requerido').isMongoId(),
   validarCampos
 ], actualizarMedicos);
 router.delete('/:id', validarJWT, borrarMedicos);
+router.get('/:id', validarJWT, getMedicoPorId);
 
 module.exports = router;

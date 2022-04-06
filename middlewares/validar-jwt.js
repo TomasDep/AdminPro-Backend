da01ea725c71f92e1c20eed18a49c07fa5d6a0a3
TomasDep <tomas.valenzuelacb@gmail.com>
2022-04-06 16:03:13 -0400

    [ADD] se crea middleware de admin_role

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 
+const Usuario = require('../models/usuario');
+
 const validarJWT = (req, res, next) => {
   // Leer token
   const token = req.header('x-token');
@@ -23,6 +25,37 @@ const validarJWT = (req, res, next) => {
   }
 }
 
+const validarAdminRole = async (req, res, next) => {
+  const uid = req.uid;
+  const id = req.params.id;
+
+  try {
+    const usuarioDB = await Usuario.findById(uid);
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        message: 'El usuario no existe'
+      });
+    }
+    
+    if (usuarioDB.role === 'ADMIN_ROLE' || uid === id) {
+      next();
+    } else {
+      return res.status(403).json({
+        ok: false,
+        message: 'Acción no autorizada'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      message: 'Error inesperado... revisar logs'
+    });
+  }
+}
+
 module.exports = {
-  validarJWT
+  validarJWT,
+  validarAdminRole
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,11 +6,11 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
-const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarJWT, validarAdminRole } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get('/', validarJWT, getUsuarios);
+router.get('/', [validarJWT, validarAdminRole], getUsuarios);
 router.post('/', [
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('password', 'La contraseña es obligatorio').not().isEmpty(),
@@ -19,11 +19,12 @@ router.post('/', [
 ], crearUsuario);
 router.put('/:id', [
   validarJWT,
+  validarAdminRole,
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('email', 'El email es obligatorio').isEmail(),
   check('role', 'El role es obligatorio').not().isEmpty(),
   validarCampos
 ], actualizarUsuario);
-router.delete('/:id', validarJWT, borrarUsuario);
+router.delete('/:id', [validarJWT, validarAdminRole], borrarUsuario);
 
 module.exports = router;
